2d8830c748ffd35e08a4eedbf98ba027d548af12
shivamanbhule47 <shivamanbhule47@gmail.com>
2026-06-27 18:54:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
-
+// vite.config.ts
+export default defineConfig({
+  base: "/lofibrosong/",
+  plugins: [react()],
+})
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
